d6d91793e022fac3ef818ecc13ccfb37140ad738
Matheus Coppi <matheuscoppi22@gmail.com>
2022-11-17 18:43:19 -0300

    building update function

--- client/src/components/edit-employee/EditEmployee.vue
+++ client/src/components/edit-employee/EditEmployee.vue
@@ -1,15 +1,65 @@
 <template>
-    <div class="employee">
-       <h1>Editar Funcionário</h1>
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h3>Editar Funcionário</h3>
+            </div>
+            <div class="card-body">
+                <form v-on:submit.prevent="updateFormEmployee()">
+                    <div class="form-group">
+                        <label class="font-weight-bold">Nome do Funcionário</label>
+                        <input type="text" id="name" class="form-control" v-model="employeeForm.name_employee">
+                    </div>
+
+                    <div class="form-group">
+                        <label class="font-weight-bold">Função do Funcionário</label>
+                        <input type="text" id="role" class="form-control" v-model="employeeForm.role_employee">
+                    </div>
+
+                    <div class="form-group">
+                        <label class="font-weight-bold">Salário do Funcionário</label>
+                        <input type="number" id="salary" class="form-control" v-model="employeeForm.salary_employee">
+                    </div>
+
+                    <div class="form-group">
+                        <label class="font-weight-bold">Número de Registro do Funcionário</label>
+                        <input type="number" id="registration" class="form-control" v-model="employeeForm.registration_employee">
+                    </div>
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary"><font-awesome-icon icon="fa-solid fa-pen" /> Update </button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
    </template>
    
    <script>
+   import ServiceUser from '../../services/user'
    export default {
        data() {
            return {
-   
+            employeeForm: {}
            }
+       },
+       mounted() {
+        this.getEmployeeById() 
+            
+        
+       },
+       
+       methods: {
+        async getEmployeeById() {
+            const {registration_employee} = this.$route.params;
+            const response = await ServiceUser.updateEmployee(registration_employee)
+            this.employeeForm = {...response}
+
+        },
+        async updateFormEmployee() {
+            await ServiceUser.updateEmployee(this.employeeForm);
+            this.$router.push ({name: 'list'})
+            },
        }
    }
    </script>
